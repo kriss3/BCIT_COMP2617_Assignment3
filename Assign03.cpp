5a9e90560395d01d36865b8832cd80a71dd8c7e0
Kris <kris_s2@outlook.com>
2019-10-05 22:04:49 -0700

    Adding comments and minor cosmetic changes to the enums; removing doRandom() as a wrapper for rand() method;

--- Assign03.cpp
+++ Assign03.cpp
@@ -1,8 +1,8 @@
 /*
 Assignment 03;
 Krzysztof Szczurowski
-October 10th 2019
-
+BCIT: A01013054
+October 1st 2019
 */
 
 #include <iostream>
@@ -10,21 +10,22 @@ October 10th 2019
 
 using namespace std;
 
-void multiplication(); // function prototype
-void correctMessage(); // function prototype
-void incorrectMessage(); // function prototype
-int doRand();
+void multiplication();			// function prototype for multiplication operations;
+void correctMessage();		// function prototype to randomize display messages for correct answers;
+void incorrectMessage();		// function prototype to randomize display messages for incorrect answers;
 
 int main()
 {
 	cout << "Enter -1 to End." << endl;
 	multiplication();
-	cout << "That's all for now. Bye.\n";
+	cout << "\nThat's all for now. Bye.\n";
 	return 0;
 }
 
+//method to perform multiplication operation;
 void multiplication() 
 {
+	//setting up required variables;
 	int exit = 0;
 	int result = 0;
 	int val1 = 0;
@@ -33,99 +34,103 @@ void multiplication()
 
 	do
 	{
-		val1 = doRand();
-		val2 = doRand();
+		//setting up random valus for multiplication operation;
+		val1 = rand() % 10;
+		val2 = rand() % 10;
 		
 		cout << "How much is " << val1 << " times " << val2 << " (-1 to End) ? ";
 		cin >> result;
 
-		while (result != val1 * val2 && result >= 0)
+		while (result != val1 * val2 && result >= 0)	// looping until the anser is correct;
 		{
-			incorrectMessage();
+			incorrectMessage();	// calling incorrectMessage() to randomly display response message;
 			cin >> result;
 		};
 		
-		if (result == val1 * val2 && result > 0)
+		if (result == val1 * val2 && result >= 0)	// checking if user answert is correct and > 0
 		{
-			correctMessage();
+			correctMessage();		//calling correctMessage() to randomly display response message;
 		}
 		
-		if (result < 0)
+		if (result < 0)			// checking for exit condition (user enters -1)
 		{
 			exit = -1;
 		}
-	} while (exit != -1);
+	} while (exit != -1);		// exit condition based user entering -1;
 }
 
 void correctMessage() 
 {
+	//enum of possible message when user answer the question correctly;
 	enum class Answer {
-		Correct = 0,
-		Very_Good,
-		Way_To_Go,
-		Fantastic,
-		You_Are_The_Best
+		CORRECT = 0,
+		VERYGOOD,
+		WAYTOGO,
+		FANTASTIC,
+		THEBEST
 	};
 
+	//randomizing selection of the enum message;
 	Answer a = static_cast<Answer>(rand() % 5);
+	
+	//base on the randomization of the enum message selecting the actual message
 	switch (a)
 	{
-	case Answer::Correct:
+	case Answer::CORRECT:
 		cout << "Correct\n\n";
 		break;
-	case Answer::Very_Good:
+	case Answer::VERYGOOD:
 		cout << "Very Good!!!\n\n";
 		break;
-	case Answer::Way_To_Go:
-		cout << "Way To Go!!!\n\n";
+	case Answer::WAYTOGO:
+		cout << "Good. Way To Go!!!\n\n";
 		break;
-	case Answer::Fantastic:
-		cout << "Fantastic!!!\n\n";
+	case Answer::FANTASTIC:
+		cout << "Correct. You are fantastic!!!\n\n";
 		break;
-	case Answer::You_Are_The_Best:
-		cout << "You Are The Best\n\n";
+	case Answer::THEBEST:
+		cout << "Good! You Are The Best\n\n";
 		break;
 	default:
-		cout << "Buuu something went wrong!\n\n";
+		cout << "Buuu something went wrong!\n\n";  //include default option as a good practice;
 		break;
 	}
-}
+}	//end correctMessage();
 
 void incorrectMessage() 
 {
+	//enum of possible message when user answer is incorrect;
 	enum class Answer {
 		INCORRECT = 0,
-		NOT_SO_GREAT,
-		NOT_GOOD,
-		NOT_QUITE_WRIGHT,
+		NOTSOGREAT,
+		NOTGOOD,
+		NOTQUITERIGHT,
 		WRONG
 	};
 
+	//randomizing selection of the enum message (incorrect message);
 	Answer a = static_cast<Answer>(rand() % 5);
+	
+	//base on the randomization of the enum message selecting the actual message
 	switch (a)
 	{
 	case Answer::INCORRECT:
 		cout << "Incorrect. Try one more time.\n? ";
 		break;
-	case Answer::NOT_SO_GREAT:
+	case Answer::NOTSOGREAT:
 		cout << "Not so great. One more time.\n? ";
 		break;
-	case Answer::NOT_GOOD:
+	case Answer::NOTGOOD:
 		cout << "Not good this time. Try again\n? ";
 		break;
-	case Answer::NOT_QUITE_WRIGHT:
-		cout << "Not quite wright. Try one more time.\n? ";
+	case Answer::NOTQUITERIGHT:
+		cout << "Not quite right. Try one more time.\n? ";
 		break;
 	case Answer::WRONG:
 		cout << "You got it wrong this time. Try again.\n? ";
 		break;
 	default:
-		cout << "Buuu something went wrong!\n? ";
+		cout << "Buuu something went wrong!\n? ";		//include default option as a good practice;
 		break;
-	}
-}
-
-int doRand()
-{
-	return rand() % 9 + 1;
-}
+	}	//end incorrectMessage();
+}	//end main;
